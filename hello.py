470b14d530673bfefa14bc66ac97a08d8f0519ad
Tor Arvid Lund <torarvid@gmail.com>
2024-10-04 19:37:57 +0200

    Test code

---
+++ hello.py
@@ -0,0 +1,9 @@
+import private_torarvid_package_1
+
+def main():
+    print("Hello from renovate-test! The above message is from private-torarvid-package-1!")
+
+
+
+if __name__ == "__main__":
+    main()
